0df5f6e67c741ef38c34f96b2f35b7847258fa33
azain4645 <47071916+azain4645@users.noreply.github.com>
2021-08-26 22:52:09 +0900

    First commit

---
+++ Vuetify_test/button-counter.vue
@@ -0,0 +1,9 @@
+// button-counter と呼ばれる新しいコンポーネントを定義します
+Vue.component('button-counter', {
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  template: '<v-btn v-on:click="count++">You clicked me {{ count }} times.</v-btn>'
+});
